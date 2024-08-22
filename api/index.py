07332de16d8c5201c77cc1151afd62bd1af74c38
mysticthrive <thrivemystic@gmail.com>
2024-08-22 12:01:50 +0500

    made some changes to index.py

--- api/index.py
+++ api/index.py
@@ -172,7 +172,4 @@ def format_markdown(markdown_text):
     # Convert Markdown to HTML
     html = markdown.markdown(markdown_text, extensions=['fenced_code'])
     return html
-    
-if __name__ == "__main__":
-    from os import environ
-    app.run(host="0.0.0.0", port=int(environ.get('PORT', 5000)))
+
